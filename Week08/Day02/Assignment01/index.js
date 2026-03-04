1b62ca863e3a59f1e2fbe73cd395a5835e4d97a1
samira <samira.saifi2000@gmail.com>
2026-03-04 11:22:02 +0430

    complete week08-day02-assignment01

--- Week08/Day02/Assignment01/index.js
+++ Week08/Day02/Assignment01/index.js
@@ -2,3 +2,23 @@ function bmiCalc(w,h){
     bmi=w/(h*h);
     return bmi;
 }
+
+let weight = prompt('enter your weight in kg');
+let height = prompt('enter your height in meters');
+
+weight = Number(weight);
+height = Number(height);
+
+let result = bmiCalc(weight, height);
+
+console.log('your BMI is : ' + result);
+
+if (result < 18.5) {
+    console.log('you are underweight.');
+}
+else if (result >= 18.5 && result < 25){
+    console.log('your weight is normal.');
+}
+else {
+    console.log('you are over overweight.')
+}
